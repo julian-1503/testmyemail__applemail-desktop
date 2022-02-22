d2c61fae7feb1ce9216be9b65c4a16b62e28158b
Julian <julian@castano.app>
2022-02-22 10:24:25 -0500

    Add comments to utility functions

--- constants.js
+++ constants.js
@@ -3,3 +3,14 @@ export const HEADER_HEIGHT = {
   768: 90,
   900: 180,
 };
+
+// The screenshot suffix for the different image sizes.
+export const SCREENSHOT_SUFFIX = {
+  smallThumbnail: "_tn",
+  largeThumbnail: "_thumb",
+  fullScreenshot: "",
+};
+
+// The guid regular expression to extract the different parts.
+export const GUID_REGEX =
+  /^((\d{4}-\d{2}-\d{2})_(((\d+)_(\d+)(_(\d+))?)(_no_images)?)_([a-zA-Z]{2,4}))/;
--- utils.js
+++ utils.js
@@ -3,18 +3,27 @@ import path from "path";
 import os from "os";
 import rimraf from "rimraf";
 import sharp from "sharp";
-import { propOr } from "ramda";
 import logger from "./Logger.js";
 import FormData from "form-data";
 import axios from "axios";
 
-const guidRegex =
-  /^((\d{4}-\d{2}-\d{2})_(((\d+)_(\d+)(_(\d+))?)(_no_images)?)_([a-zA-Z]{2,4}))/;
+import { GUID_REGEX } from "./constants.js";
 
+/**
+ * Get the root folder of the application.
+ *
+ * @return {String} an absolute path to the folder.
+ */
 export const getRootFolder = () => {
   return path.join(os.homedir(), "Documents", "applemail");
 };
 
+/**
+ * Create an .eml file from the test contents.
+ * @param {String} name - the expected file name.
+ * @param {String} content - the base64 encoded string containing the email contents.
+ *
+ */
 export const createEMLFile = (name, content) => {
   const dir = path.join(getRootFolder(), "eml");
 
@@ -28,12 +37,28 @@ export const createEMLFile = (name, content) => {
   fs.writeFileSync(path.join(dir, name), decoded);
 };
 
+/**
+ * Resize the image to a full screenshot.
+ *
+ * @param {Buffer} screenshot - the original screenshot Buffer.
+ *
+ * @return {Buffer} the resulting image after resize.
+ */
 export const resizeFull = async (screenshot) => {
   const buffer = await sharp(screenshot).toBuffer();
 
   return buffer;
 };
 
+/**
+ * Resize the image to a smalll thumbnail.
+ *
+ * @param {Buffer} screenshot - the original screenshot Buffer.
+ * @param {Number} endWidth - the expected width of the thumbnail.
+ * @param {Number} endHeight - the expected height of the thumbnail.
+ *
+ * @return {Buffer} the resulting image after resize.
+ */
 export const resizeSmall = async (screenshot, endWidth, endHeight) => {
   const buffer = await sharp(screenshot)
     .resize({
@@ -42,26 +67,29 @@ export const resizeSmall = async (screenshot, endWidth, endHeight) => {
       background: { r: 0, g: 0, b: 0, alpha: 0 },
       position: "left top",
     })
-    .toBuffer()
-    .then((data) => {
-      return sharp(data)
-        .metadata()
-        .then(({ width, height }) => {
-          return sharp(data)
-            .resize({ height, position: "left top" })
-            .extract({
-              top: 0,
-              left: 0,
-              width: width > endWidth ? endWidth : width,
-              height: height > endHeight ? endHeight : height,
-            })
-            .toBuffer();
-        });
-    });
+    .toBuffer();
 
-  return buffer;
+  const { width, height } = await sharp(buffer).metadata();
+
+  return sharp(buffer)
+    .resize({ height, position: "left top" })
+    .extract({
+      top: 0,
+      left: 0,
+      width: width > endWidth ? endWidth : width,
+      height: height > endHeight ? endHeight : height,
+    })
+    .toBuffer();
 };
 
+/**
+ * Resize the image to a large thumbnail.
+ *
+ * @param {Buffer} screenshot - the original screenshot Buffer.
+ * @param {Number} endWidth - the expected with of the Thumbnail.
+ *
+ * @return {Buffer} the resulting image after resize.
+ */
 export const resizeBig = async (screenshot, endWidth) => {
   const buffer = await sharp(screenshot)
     .resize({
@@ -73,28 +101,66 @@ export const resizeBig = async (screenshot, endWidth) => {
   return buffer;
 };
 
+/**
+ * Resize original screeenshot to a thumbnail with fixed dimensions.
+ *
+ * @param {Buffer} screenshot - the original screenshot Buffer.
+ * @param {Number} width - the small thumbnail width.
+ * @param {Number} height - the small thumbnail height.
+ * @param {Function} resize - the resize implementation.
+ *
+ * @return {Buffer} the resulting resized image.
+ */
 export const createThumbnail = (screenshot, width, height, resize) => {
   return resize(screenshot, width, height);
 };
 
+/**
+ * Divide the GUID into its different components.
+ *
+ * @param {String} guid - the legacy GUID Format.
+ *
+ * @return {Array}  a list of all the different parts.
+ */
 const extractAllPartsFromGuid = (guid) => {
-  const result = guidRegex.exec(guid);
+  const result = GUID_REGEX.exec(guid);
 
   return result || [];
 };
 
+/**
+ * Extract the zone from the GUID.
+ *
+ * @param {String} guid - the legacy GUID Format.
+ *
+ * @return {String} the zome if present, an empty string otherwise.
+ */
 const getZoneFromGuid = (guid) => {
   const result = extractAllPartsFromGuid(guid);
 
   return result[10] || "";
 };
 
+/**
+ * Extract the test sent date from the GUID.
+ *
+ * @param {String} guid - the legacy GUID Format.
+ *
+ * @return {String} the sent date if present, an empty string otherwise.
+ */
 export const getDateFromGuid = (guid) => {
   const result = extractAllPartsFromGuid(guid);
 
   return result[2] || "";
 };
 
+/**
+ * Extract the test sent time from the GUID.
+ *
+ * @param {String} guid - the legacy GUID Format.
+ *
+ * @return {String} the sent time if present, an empty string otherwise.
+ */
 export const getTestTimeFromGuid = (guid) => {
   const result = extractAllPartsFromGuid(guid);
   const time = result[8] || result[6];
@@ -102,29 +168,48 @@ export const getTestTimeFromGuid = (guid) => {
   return (time || "").replace(/(.{2})/g, "$1:").slice(0, -1);
 };
 
+/**
+ * Extract the member id from the legacy GUID.
+ *
+ * @param {String} guid - the legacy GUID Format.
+ *
+ * @return {String} the member id if present, an empty string otherwise.
+ */
 export const getMemberIdFromGuid = (guid) => {
   const result = extractAllPartsFromGuid(guid);
 
   return result[5] || "";
 };
 
+/**
+ * Whether the test wants to check for Image Blocking.
+ *
+ * @param {String} guid - the legacy GUID Format.
+ *
+ * @return {String} - "Y" if is for image blocking, empty stirng otherwise.
+ */
 export const testHasBlockedImages = (guid) => {
   const hasImageBlock = guid.indexOf("_no_images") >= 0;
 
   return hasImageBlock ? "Y" : "";
 };
 
-export const getFileName = (
-  guid,
-  test_guid,
-  clientFolder,
-  imgFormat,
-  suffix
-) => {
+/**
+ * Format the screenshot URL to become the path at S3.
+ *
+ * @param {String} guid - the legacy GUID Format.
+ * @param {String} ssGuid - the new SSGUID.
+ * @param {String} clientFolder - the image folder identifier.
+ * @param {String} imgFormat - the image type (png, jpg).
+ * @param {String} suffix - the screenshot suffix depending on the image size.
+ *
+ * @return {String} the image path for S3.
+ */
+export const getFileName = (guid, ssGuid, clientFolder, imgFormat, suffix) => {
   const zone = getZoneFromGuid(guid);
   const hasImageBlock = guid.indexOf("_no_images") >= 0;
 
-  return `${zone}/${test_guid}/${clientFolder}${suffix}${
+  return `${zone}/${ssGuid}/${clientFolder}${suffix}${
     hasImageBlock ? "_no_images" : ""
   }.${imgFormat}`;
 };
